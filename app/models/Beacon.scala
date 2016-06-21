89268b1350000db943be6713c528cbde4367c05e
Taylor Ninesling <t9sling@hotmail.com>
2016-06-21 19:15:34 -0400

    Added Point class to implement GeoJSON specifications; This is the intially compiled base build

--- app/models/Beacon.scala
+++ app/models/Beacon.scala
@@ -1,11 +1,22 @@
-package models;
+package models
+
+import java.util.Date
 
 import play.api.libs.iteratee.Iteratee
 
 import reactivemongo.api.collections.bson.BSONCollection
 import reactivemongo.bson.BSONDocument
+import reactivemongo.bson.BSONObjectID
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
-class Beacon {}
+/** This is the class defining Beacons stored in the database
+  *
+  * @constructor creates a Beacon
+  * @param
+  */
+case class Beacon(id: Option[BSONObjectID], creator: String, title: String,
+                  location: Point, startTime: Date, endTime: Date, range: Double,
+                  placeName: String, address: String, tags: List[String],
+                  notifiedCount: Int, notified: List[String])
