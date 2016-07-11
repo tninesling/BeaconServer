969fd15c41c0395f9351d63dbf14234f79865498
Taylor Ninesling <t9sling@hotmail.com>
2016-07-10 22:59:27 -0400

    Defined MongoService for connection to the database

--- app/services/MongoService.scala
+++ app/services/MongoService.scala
@@ -4,8 +4,25 @@ import javax.inject.Inject
 
 import play.api.Configuration
 
+import reactivemongo.api.MongoConnection
 import reactivemongo.api.MongoDriver
+import reactivemongo.api.collections.bson.BSONCollection
+
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class MongoService @Inject()(config: Configuration) {
-  
+  // if no host names found, default to localhost:27017
+  val hostURI = "mongodb://" + config.getString("localmongodb.server")
+                                          .getOrElse("localhost:27017")
+                                          .toString()
+
+  // if no db name found, default to test
+  val dbName = config.getString("mongodb.db").getOrElse("test")
+
+  val driver = new MongoDriver
+  val connection = driver.connection(List(hostURI))
+  val database = connection.database(dbName)
+
+  val beacons = database.map(_.collection("beacons"))
+  val users = database.map(_.collection("users"))
 }
